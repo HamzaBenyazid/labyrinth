d0ce581eba6b8a7b284a42f172aa3362ecac0514
Hamza Benyazid <benyazidhamza969@gmail.com>
2021-01-04 13:49:55 +0100

    add main.c

--- src/Affichage.c
+++ src/Affichage.c
@@ -52,27 +52,4 @@ void fill(cell* m,int i,int j,int up, int right, int down, int left)
     (m+N*i+j)->down = down;
     (m+N*i+j)->left = left;
 }
-int main()
-{
-    matriceDesCell m=(cell*)malloc(N*N*sizeof(cell));
-    init(&m);
-    fill(m,0,0,-1,1,0,-1);
-    fill(m,0,1,-1,0,1,1);
-    fill(m,0,2,-1,1,0,0);
-    fill(m,0,3,-1,-1,1,1);
-    fill(m,1,0,0,0,1,-1);
-    fill(m,1,1,1,1,0,0);
-    fill(m,1,2,0,1,0,1);
-    fill(m,1,3,1,-1,1,1);
-    fill(m,2,0,1,1,1,-1);
-    fill(m,2,1,0,1,0,1);
-    fill(m,2,2,0,0,0,1);
-    fill(m,2,3,1,-1,1,0);
-    fill(m,3,0,1,1,-1,-1);
-    fill(m,3,1,0,1,-1,1);
-    fill(m,3,2,0,1,-1,1);
-    fill(m,3,3,1,-1,-1,1);
-    char* matrix_2_show=matrix2show(m);
-    affichage(matrix_2_show);
-    return 0;
-}
+
